1146aeaaf2425ac199ac23cdc34feb04aa165210
Chiyang Wan <chiyang10000@gmail.com>
2019-08-29 16:58:30 +0800

    Add GitHub Actions

---
+++ .github/workflows/scripts/init_hdfs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+
+
+# Install
+brew install hadoop
+
+# Configure
+export HADOOP_HOME=/usr/local/opt/hadoop/libexec
+tee $HADOOP_HOME/etc/hadoop/core-site.xml << EOF
+<configuration>
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://localhost:8020</value>
+    </property>
+    <property>
+         <name>hadoop.proxyuser.admin.hosts</name>
+         <value>*</value>
+    </property>
+    <property>
+        <name>hadoop.proxyuser.admin.groups</name>
+        <value>*</value>
+    </property>
+</configuration>
+EOF
+tee $HADOOP_HOME/etc/hadoop/hdfs-site.xml << EOF
+<configuration>
+    <property>
+        <name>dfs.namenode.name.dir</name>
+        <value>file:///db_data/hdfs/name</value>
+    </property>
+    <property>
+        <name>dfs.datanode.data.dir</name>
+        <value>file:///db_data/hdfs/data</value>
+    </property>
+    <property>
+        <name>dfs.replication</name>
+        <value>1</value>
+    </property>
+</configuration>
+EOF
+
+# Initialize
+install -d /db_data/hdfs/name
+install -d /db_data/hdfs/data
+hdfs namenode -format
+
+# Start
+/usr/local/opt/hadoop/sbin/start-dfs.sh
+
+# Connect
+hdfs dfsadmin -report
+hdfs dfs -ls /
