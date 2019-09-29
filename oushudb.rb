04b16f5c6d89138ba973259320120fbd322fea3c
Chiyang Wan <chiyang10000@gmail.com>
2019-09-29 12:21:56 +0800

    Update OushuDB-2019-09-29

--- oushudb.rb
+++ oushudb.rb
@@ -1,7 +1,8 @@
 require "formula"
 
 class Oushudb < Formula
-  url "http://yum.oushu-tech.com/oushurepo/yumrepo/internal/linux/toolchain/OushuDB-Darwin.tar.xz"
+  url "http://yum.oushu-tech.com/oushurepo/yumrepo/internal/linux/toolchain/OushuDB-Darwin-2019-09-29.tar.xz"
+  sha256 "1937e2ff8cd8fafd1302afa67d59083e39cde51f3a8ded0e69748d0ff2c563f9"
   version "4.X"
 
   depends_on "ossp-uuid"
