c1922acffc857381703b12db3b51b0684cc5d479
Chiyang Wan <chiyang10000@gmail.com>
2020-12-06 11:13:41 +0800

    Update OushuDB-2020-12-08

--- .github/workflows/scripts/test_oushudb.sh
+++ .github/workflows/scripts/test_oushudb.sh
@@ -10,21 +10,15 @@ select version();
 select gp_opt_version();
 
 create table t_ao(i int);
-create table t_orc(i int) format 'orc';
-create table t_magmatp(i int) format 'magmatp';
-create table t_magmaap(i int) format 'magmaap';
+create table t_orc(i int) with (appendonly=true,orientation=orc);
 create table t_parquet(i int) with (appendonly=true,orientation=parquet);
 
 insert into t_ao select generate_series(1,1000);
 insert into t_orc select generate_series(1,1000);
-insert into t_magmatp select generate_series(1,1000);
-insert into t_magmaap select generate_series(1,1000);
 insert into t_parquet select generate_series(1,1000);
 
 select count(*) from t_ao;
 select count(*) from t_orc;
-select count(*) from t_magmatp;
-select count(*) from t_magmaap;
 select count(*) from t_parquet;
 EOF
 
--- oushudb.rb
+++ oushudb.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class Oushudb < Formula
-  url "http://yum.oushu-tech.com/oushurepo/yumrepo/internal/linux/toolchain/OushuDB-Darwin-2020-05-21.tar.xz"
-  sha256 "c9cc8d649755af3318daaf6da68b3d673c0b246145971393f8fa99a4ad99e8da"
-  version "4.X"
+  url "http://yum.oushu-tech.com/oushurepo/yumrepo/internal/linux/toolchain/OushuDB-Darwin-2020-12-08.tar.xz"
+  sha256 "04065ce5d0f9fb8627a230902b38322a14af9f72451ea7ab3502326e1dff630c"
+  version "2020-12-08"
 
   depends_on "hadoop"
   keg_only ""
