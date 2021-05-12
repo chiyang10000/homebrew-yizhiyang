161326add9bba2a13680ae08ee69699953a05ec7
Chiyang Wan <chiyang10000@gmail.com>
2021-05-12 11:03:42 +0800

    debug

--- oushudb.rb
+++ oushudb.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Oushudb < Formula
-  url "http://yum.oushu-tech.com/oushurepo/yumrepo/internal/linux/toolchain/OushuDB-Darwin-2020-12-08.tar.xz"
+  url "http://yumazure.oushu-tech.com:12000/oushurepo/yumrepo/internal/linux/toolchain/bak/OushuDB-Darwin-2020-12-08.tar.xz"
   sha256 "04065ce5d0f9fb8627a230902b38322a14af9f72451ea7ab3502326e1dff630c"
   version "2020-12-08"
 
